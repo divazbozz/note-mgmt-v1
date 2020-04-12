14e6bdab4c4d2c4e5455cda471804f961f896978
DZ <davidbozzman@gmail.com>
2020-04-11 17:18:01 -0700

    Updates

---
+++ precache-manifest.9433049f36779d87a6ccb682d9fa2854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5319c05dcbdc2aa81c2e654a2bfdd901",
+    "url": "/divazbozz/note-mgmt-v1/index.html"
+  },
+  {
+    "revision": "8161af51e65de681ddb4",
+    "url": "/divazbozz/note-mgmt-v1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "43c67ef1508aebe37a41",
+    "url": "/divazbozz/note-mgmt-v1/static/js/2.5bb2e13b.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/divazbozz/note-mgmt-v1/static/js/2.5bb2e13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8161af51e65de681ddb4",
+    "url": "/divazbozz/note-mgmt-v1/static/js/main.02db42fb.chunk.js"
+  },
+  {
+    "revision": "f1b19db0907b2764ce64",
+    "url": "/divazbozz/note-mgmt-v1/static/js/runtime-main.ec36c4ab.js"
+  }
+]);
